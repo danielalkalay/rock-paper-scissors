4cf000d036ba2919aeeda393f723dc1d65d078d3
daniel <danielalkalay4@gmail.com>
2022-02-03 17:53:35 +0200

    final version

--- app.js
+++ app.js
@@ -20,103 +20,90 @@ const scissors_div = document.querySelector("#scissors");
 
 // functions
 
-// finish and reset functions
-
-newGame_div.addEventListener("click", () => reset());
-
-const reset = () => {
-  computerScore = 0;
-  userScore = 0;
-  userScore_span.innerText = userScore;
-  computerScore_span.innerText = computerScore;
-  choices_div.style.display = "flex";
-  endGame_div.style.display = "none";
-  result_div.innerText = "do your first move";
-};
-
-const finish = () => {
-  if ((userScore === 5) | (computerScore === 5)) {
-    if (userScore === 5) {
-      result_div.innerText = "you are the winer out of five games";
-      choices_div.style.display = "none";
-      endGame_div.style.display = "flex";
-      newGame_div.style.display = "flex";
-    } else {
-      result_div.innerText = "the computer is the winer out of five games";
-      choices_div.style.display = "none";
-      endGame_div.style.display = "flex";
-      newGame_div.style.display = "flex";
-    }
-  }
-};
-
-//conclusion functions
-
-const win = () => {
-  userScore++;
-  userScore_span.innerText = userScore;
-  result_div.innerText = `you won`;
-  finish();
-};
-
-const lose = () => {
-  computerScore++;
-  computerScore_span.innerText = computerScore;
-  result_div.innerText = `computer  win , try again`;
-  finish();
-};
-
-const draw = () => {
-  result_div.innerText = "it's a draw, both of you chose the same";
-  finish();
-};
-
-//Round
-
-const playRound = (userChoice) => {
-  const computerChoice = getComputerChoice();
-  console.log(userChoice);
-  userImg.src = `./images/${userChoice}.png`;
-  compImg.src = `./images/${computerChoice}.png`;
-
-  const round = `${userChoice + computerChoice}`;
-
-  switch (round) {
-    case "rockscissors":
-    case "paperrock":
-    case "scissorspaper":
-      win(userChoice, computerChoice);
-      break;
-    case "scissorsrock":
-    case "rockpaper":
-    case "paperscissors":
-      lose();
-      break;
-    // case userChoice === computerChoice:
-    case "scissorsscissors":
-    case "rockrock":
-    case "paperpaper":
-      draw();
-      break;
-  }
-};
-
-// computer random choice
-
-const getComputerChoice = () => {
-  const choices = ["rock", "paper", "scissors"];
-  const randomNum = Math.floor(Math.random() * 3);
-
-  return choices[randomNum];
-};
-
-// show choice pic
-
-// the three options that you choose of, after a choice, in the main function
-// callback function -playRound- r/p/s = user choice
-//
-
 const main = () => {
+  // finish and reset functions
+
+  newGame_div.addEventListener("click", () => reset());
+
+  const reset = () => {
+    computerScore = 0;
+    userScore = 0;
+    userScore_span.innerText = userScore;
+    computerScore_span.innerText = computerScore;
+    choices_div.style.display = "flex";
+    endGame_div.style.display = "none";
+    result_div.innerText = "Do your first move";
+  };
+
+  const finish = () => {
+    if (userScore === 5 || computerScore === 5) {
+      if (userScore === 5) {
+        result_div.innerText = "you are the winner out of five games";
+        choices_div.style.display = "none";
+        endGame_div.style.display = "flex";
+        newGame_div.style.display = "flex";
+      } else {
+        result_div.innerText = "the computer is the winner out of five games";
+        choices_div.style.display = "none";
+        endGame_div.style.display = "flex";
+        newGame_div.style.display = "flex";
+      }
+    }
+  };
+
+  //Round
+  //conclusion functions
+
+  const playRound = (userChoice) => {
+    const computerChoice = getComputerChoice();
+    console.log(userChoice);
+    userImg.src = `./images/${userChoice}.png`;
+    compImg.src = `./images/${computerChoice}.png`;
+
+    const round = `${userChoice + computerChoice}`;
+
+    switch (round) {
+      case "rockscissors":
+      case "paperrock":
+      case "scissorspaper":
+        // win();
+        userScore++;
+        userScore_span.innerText = userScore;
+        result_div.innerText = `you chose ${userChoice} that beats the ${computerChoice}`;
+        break;
+      case "scissorsrock":
+      case "rockpaper":
+      case "paperscissors":
+        // lose();
+        computerScore++;
+        computerScore_span.innerText = computerScore;
+        result_div.innerText = `computer  win with ${computerChoice} that beat your ${userChoice} , try again`;
+        break;
+      // case userChoice === computerChoice:
+      case "scissorsscissors":
+      case "rockrock":
+      case "paperpaper":
+        // draw();
+        result_div.innerText = `it's a draw, both of you chose ${userChoice}`;
+
+        break;
+    }
+    finish();
+  };
+
+  // computer random choice
+
+  const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNum = Math.floor(Math.random() * 3);
+
+    return choices[randomNum];
+  };
+
+  // the three options that you choose of, after a choice, in the main function
+  // callback function -playRound => -getComputerChoice- -finish-? => -reset-
+  //
+
   rock_div.addEventListener("click", () => playRound("rock"));
   paper_div.addEventListener("click", () => playRound("paper"));
   scissors_div.addEventListener("click", () => playRound("scissors"));
